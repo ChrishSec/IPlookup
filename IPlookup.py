9808ca22a5a676166a6e76403e38ab307f101572
ChrishSec <67695405+ChrishSec@users.noreply.github.com>
2024-03-20 12:20:53 -0400

    Update IPlookup.py

--- IPlookup.py
+++ IPlookup.py
@@ -30,6 +30,7 @@ def main():
             headers = {
                 "Host": "iplookup.chrishsec.com",
                 "User-Agent": "IPlookup/v1.0.0",
+                "Authorization": "Bearer 3ca64848-8bce-41a3-9dbb-3fecf583b85e",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
                 "Accept-Language": "en-US,en;q=0.5",
                 "Accept-Encoding": "gzip, deflate",
